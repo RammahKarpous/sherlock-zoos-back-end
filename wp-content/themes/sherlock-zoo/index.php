1dd7184aec021fb8c67cd416715b2483eca0dfef
Rammah Karpous <35765926+RammahKarpous@users.noreply.github.com>
2021-04-13 15:45:51 +0100

    Looped through the content

--- wp-content/themes/sherlock-zoo/index.php
+++ wp-content/themes/sherlock-zoo/index.php
@@ -1,5 +1,16 @@
 <?php get_header(); ?>
 
-
+<div class="main">
+    <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+        <section class="post-content">
+            <h2><?php the_title() ?></h2>
+            <p><?php the_content() ?></p>
+            <small><?php the_time('F j, Y') ?></small>
+        </section>
+    <?php endwhile;  
+        else : ?>
+            <p>There are no posts</p>
+    <?php endif; ?>
+</div>
 
 <?php get_footer();
