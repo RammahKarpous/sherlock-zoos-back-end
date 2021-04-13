9be334c0575e039fc89c5a05b8dbd16c0e1ce327
Rammah Karpous <35765926+RammahKarpous@users.noreply.github.com>
2021-04-13 14:39:41 +0100

    Creating the theme and registered style.css

--- wp-content/themes/sherlock-zoo/functions.php
+++ wp-content/themes/sherlock-zoo/functions.php
@@ -1,7 +1,16 @@
 <?php
 
-add_action('init', 'init_remove_support',100);
+// Registering stylesheet
+function theme_add_stylesheet() {
+    wp_enqueue_style( 'mainstyle', get_stylesheet_uri() );
+}
+
+add_action( 'wp_enqueue_scripts', 'mainstyles' );
+
+// Removing the default editor
 function init_remove_support(){
     $post_type = 'your post type';
     remove_post_type_support( $post_type, 'editor');
 }
+
+add_action('init', 'init_remove_support',100);
